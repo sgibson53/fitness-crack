3cb633f8ac59698b52111e88e564fc050cc5a834
Seth Gibson <seth_gibson@oxy.com>
2018-03-15 16:00:41 -0500

    old auth cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'app';
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    // this.http
-    //   .get('http://localhost:3000/setup')
-    //   .subscribe(res => console.log(res));
-  }
-
-  authenticate() {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        Authorization: 'my-auth-token'
-      })
-    };
-    this.http
-      .post(
-        'http://localhost:3000/authenticate',
-        { name: 'Seth Gibson', password: 'password', admin: true },
-        httpOptions
-      )
-      .subscribe(res => console.log(res));
-  }
+export class AppComponent {
+  constructor() {}
 }
